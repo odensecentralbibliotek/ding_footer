c76d3114c2b159a12050d6620e1e3d6a47be5ed3
Ulrich Christensen <ubcudvikler@gmail.com>
2012-09-10 12:54:34 +0200

    Display footer blocks flat

--- ding_footer.tpl.php
+++ ding_footer.tpl.php
@@ -7,11 +7,11 @@ $num_footer_blocks = count($footer_blocks);
 <div class="ding-footer-wrapper">
 <?php
   
-foreach($footer_blocks as $key => $value) {
+foreach($footer_blocks as $key => $foot) {
   
 ?>
   <div class="ding-footer-column footer-block-<?php echo ($key + 1);?>-of-<?php echo $num_footer_blocks; ?>">
-<?php /*render($value['content']);*/ ?>
+<?php $foot['content']; ?>
   </div>
 <?php
 
