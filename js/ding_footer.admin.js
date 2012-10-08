1fd5ef1558c0701f4a97617adef0f39d9583a911
Kristian Kaa <kaakristian@gmail.com>
2012-10-08 14:55:06 +0200

    Added JS to stored the state of textfield on AJAX

--- js/ding_footer.admin.js
+++ js/ding_footer.admin.js
@@ -1,4 +1,15 @@
 (function ($) {
-  $('.load_wysiwyg').ckeditor();
-  console.log('done');
+  Drupal.behaviors.ding_footer_callback = {
+    attach: function (context, settings) {
+      $('#edit-submit').click(function() {     
+         $('.wysiwyg-ajax').each(function() {               
+           var id = $(this).attr('id');
+           var data = CKEDITOR.instances[id].getData();           
+           $(this).val(data);
+         });
+      });
+    }
+  };
+  
 })(jQuery);
+
